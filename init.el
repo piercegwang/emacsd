833bc3ff0e2efe2e832c8b044b4af49405222b32
piercegwang <piercegwang@users.noreply.github.com>
2018-11-01 23:16:14 -0700

    init.el reorganization

--- init.el
+++ init.el
@@ -1,89 +1,32 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes (quote (smart-mode-line-powerline)))
- '(custom-safe-themes
-   (quote
-    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(default-input-method "TeX")
- '(epg-gpg-program "/usr/local/bin/gpg")
- '(erc-modules
-   (quote
-    (autojoin button completion fill irccontrols list log match menu move-to-prompt netsplit networks noncommands readonly ring stamp track)))
- '(org-agenda-files
-   (list "~/Dropbox/org/school.org" "~/Dropbox/org/todo.org" "~/Dropbox/org/violin.org"))
- '(org-modules
-   (quote
-    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill org-learn)))
- '(org-structure-template-alist
-   (quote
-    (("s" "#+BEGIN_SRC ?
+;; init.el by Pierce Wang
 
-#+END_SRC")
-     ("e" "#+BEGIN_EXAMPLE
-?
-#+END_EXAMPLE")
-     ("q" "#+BEGIN_QUOTE
-?
-#+END_QUOTE")
-     ("v" "#+BEGIN_VERSE
-?
-#+END_VERSE")
-     ("V" "#+BEGIN_VERBATIM
-?
-#+END_VERBATIM")
-     ("c" "#+BEGIN_CENTER
-?
-#+END_CENTER")
-     ("C" "#+BEGIN_COMMENT
-?
-#+END_COMMENT")
-     ("l" "#+BEGIN_EXPORT latex
-?
-#+END_EXPORT")
-     ("L" "#+LaTeX: ")
-     ("h" "#+BEGIN_EXPORT html
-?
-#+END_EXPORT")
-     ("H" "#+HTML: ")
-     ("a" "#+BEGIN_EXPORT ascii
-?
-#+END_EXPORT")
-     ("A" "#+ASCII: ")
-     ("i" "#+INDEX: ?")
-     ("I" "#+INCLUDE: %file ?"))))
- '(package-selected-packages
-   (quote
-    (elpy exec-path-from-shell smart-mode-line-powerline-theme smart-mode-line latex auctex evil-visual-mark-mode)))
- '(safe-local-variable-values
-   (quote
-    ((eval face-remap-add-relative
-	   (quote default)
-	   :family "Arial"))))
- '(show-paren-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; include paths / dirs
+(add-to-list 'load-path "~/.emacs.d/custom_load/")
+(setq init-dir "~/.emacs.d/init_files/")
+(load (concat init-dir "custom.el"))
+;;; exec-path-from-shell-initialize
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; packages
 (require 'package)
-;;Install melpa
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(setq package-archives
+    '(("melpa-stable" . "https://stable.melpa.org/packages/")
+      ("gnu" . "https://elpa.gnu.org/packages/")
+      ))
 (package-initialize)
-;;evil mode
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; evil mode
 (add-to-list 'load-path "~/.emacs.d/site-lisp/evil")
 (require 'evil)
 (evil-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Chinese Fonts
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Fonts
 (if (and (fboundp 'daemonp) (daemonp))
     (add-hook 'after-make-frame-functions
               (lambda (frame)
@@ -91,10 +34,16 @@
                   (set-fontset-font "fontset-default"
                                     'chinese-gbk "WenQuanYi Micro Hei Mono 14"))))
   (set-fontset-font "fontset-default" 'chinese-gbk "WenQuanYi Micro Hei Mono 14"))
+(set-default-font "Source Code Pro")
+(set-face-attribute 'variable-pitch
+                     nil
+                     :family "Source Code Pro")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; elpy
 (elpy-enable)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Org Configuration
 (require 'org)
 (setq org-directory "~/Dropbox/org/")
@@ -120,67 +69,76 @@
                 (lambda () (interactive) (find-file (concat org-directory "/links.org"))))
 (setq org-agenda-overriding-columns-format "%28ITEM %TODO %SCHEDULED %DEADLINE %TAGS")
 
-;; org-drill
-(add-to-list 'load-path "~/.emacs.d/custom_load/")
-(require 'org-drill)
-;; org-capture-templates
-
+;;; Quick Capture
 (setq org-capture-templates
  '(("t" "Todo" entry (file+headline "~/Dropbox/org/todo.org" "Tasks")
-;;        "* TODO %?\n  %i\n  %a")
         "* TODO %?")
    ("L" "Link" entry (file "~/Dropbox/org/links.org")
-        "* TOREAD %?[[%:link][%:description]] %U\n" :prepend t)))
+    "* TOREAD %?[[%:link][%:description]] %U\n" :prepend t)))
 
-;; cdlatex
-(load "~/.emacs.d/custom_load/cdlatex.el")
-(define-key org-cdlatex-mode-map (kbd "C-c M-d") 'cdlatex-dollar)
-(define-key cdlatex-mode-map (kbd "C-c M-d") 'cdlatex-dollar)
+;;; org-drill
+(require 'org-drill)
 
-;; Setting default frame settings
-(add-to-list 'default-frame-alist '(height . 46))
-(add-to-list 'default-frame-alist '(width . 146))
-(set-default-font "Source Code Pro")
-(set-face-attribute 'variable-pitch
-                     nil
-                     :family "Source Code Pro")
-
-;; Agenda key (C-c a) and other settings
+;;; Agenda key (C-c a) and other settings
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-switchb)
 
-;; Visual line mode (for text wrapping)
+;;; org-format-latex-options
+(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.4))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; cdlatex
+(load "~/.emacs.d/custom_load/cdlatex.el")
+(define-key org-cdlatex-mode-map (kbd "C-c M-d") 'cdlatex-dollar)
+(define-key cdlatex-mode-map (kbd "C-c M-d") 'cdlatex-dollar)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Visuals
+;;; Frame
+(add-to-list 'default-frame-alist '(height . 46))
+(add-to-list 'default-frame-alist '(width . 146))
+
+;;; Visual line mode (for text wrapping)
 (global-visual-line-mode t)
 (global-linum-mode t)
 (set-default 'truncate-lines t)
 
-;; Include Texbin in PATH
-;;(setq exec-path (append exec-path '("/Library/TeX/texbin")) )
-;;(setenv "PATH" "/usr/local/bin:/Library/TeX/texbin:$PATH" t)
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
-(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.4))
-
-;; smart-mode-line
+;;; smart-mode-line
 (require 'smart-mode-line)
 (load-theme 'tango-dark t)
 (setq sml/theme 'powerline)
 (sml/setup)
 
+;; Make title bar dark
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark)) ;; assuming you are using a dark theme
+;;(setq ns-use-proxy-icon nil)
+;;(setq frame-title-format nil)
+;;(put 'dired-find-alternate-file 'disabled nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; GPG
 (require 'epa-file)
-
 (epa-file-enable)
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; In case future me wants to change bash version
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General Tweaks
+
+;;; Change shell process (from bash to zsh)
 (setq shell-file-name "/bin/zsh")
 (put 'scroll-left 'disabled nil)
 
-;; Increment Numbers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Custom Functions
+
+;;; Increment Numbers
 (defun increment-number-at-point ()
   "Increments numbers at cursor"
   (interactive)
@@ -190,20 +148,9 @@
   (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
 (global-set-key (kbd "C-; C-=") 'increment-number-at-point)
 
-;; Kbd Macros
-(fset 'setupworkspace
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("aao23iooopo" 0 "%d")) arg)))
-(global-set-key (kbd "C-x C-k 1") 'setupworkspace)
-
-(defun insertdirectory () (interactive) (insert default-directory))
-
-(fset 'OHSFigureSave
-   [?# ?+ ?C ?A ?P ?T ?I ?O ?N ?: ?  ?\C-x ?Q return return tab ?\[ ?\[ ?f ?i ?l ?e ?: ?. ?/ ?W ?e ?e ?k ?  ?\C-x ?Q return ?/ ?\C-x ?Q return ?_ ?\C-u ?\M-! ?d ?a ?t ?e ?  ?+ ?% ?H ?% ?M ?% ?S return escape ?e ?a ?. ?p ?n ?g escape ?v ?B ?F ?/ ?l ?y escape ?A ?\] ?\] return escape ?p ?0 ?i ?\M-x ?i ?n ?s ?e ?r ?t ?d ?i ?r ?e ?c ?t ?o ?r ?y return escape ?V ?d ?i ?\C-x ?\C-f ?\C-  ?\C-a backspace ?/ ?U ?s ?e ?r ?s ?/ ?p ?i ?e ?r ?c ?e ?w ?a ?n ?g ?/ ?S ?c ?r ?e ?e ?n ?s ?h ?o ?t ?s return ?s ?\M-< ?/ ?S ?c ?r ?e ?e ?n ?  ?S ?h ?o ?t return ?R ?\C-  ?\C-a backspace ?\s-v backspace return ?\C-x ?k return])
-(global-set-key (kbd "<f9>") 'OHSFigureSave)
-
-(fset 'importChineseFlashcards
-   [return ?\C-p ?* ?* ?  ?I ?t ?e ?m ?\C-c ?\C-c ?d ?r ?i ?l ?l return ?\C-n ?\C-a ?\C-z ?f ?= ?x ?x ?\C-z ?\C-k ?\C-n ?\C-a return return ?\C-p ?* ?* ?  ?A ?n ?s ?w ?e ?r ?\C-a ?* ?\C-n ?\C-a ?\C-y ?\; ?  ?\C-a ?\C-n ?\C-n])
-(global-set-key (kbd "<f6>") 'importChineseFlashcards)
+(defun insertdirectory ()
+  "Insert current directory for macro use"
+  (interactive) (insert default-directory))
 
 (defun my-macro-query (arg)
   "Prompt for input using minibuffer during kbd macro execution.
@@ -220,33 +167,13 @@ If the input is non-empty, it is inserted at point."
     (unless (string= "" input) (insert input))))
 (global-set-key "\C-xQ" 'my-macro-query)
 
-;; Run Emacs as Daemon
-(server-start)
-;; ERC
-(setq erc-log-channels-directory "~/logs/")
-(setq erc-save-buffer-on-part t)
-
-;; Trying to get rid of title bar
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark)) ;; assuming you are using a dark theme
-;;(setq ns-use-proxy-icon nil)
-;;(setq frame-title-format nil)
-(put 'dired-find-alternate-file 'disabled nil)
-
-;; Set keys only for Mac OSX
-(when (eq system-type 'darwin)
-    (setq mac-option-modifier 'meta)
-    (setq mac-control-modifier 'control)
-    (setq ns-function-modifier 'hyper))
-
-;; Moving Windows with Hyper (Fn)
+;;; Switching Frames
 (defun hyper-window-left (count)
   "Move the cursor to new COUNT-th window left of the current one."
   :repeat nil
   (interactive "p")
   (dotimes (i count)
     (windmove-left)))
-(global-set-key (kbd "H-h") 'hyper-window-left)
 
 (defun hyper-window-right (count)
   "Move the cursor to new COUNT-th window right of the current one."
@@ -254,7 +181,6 @@ If the input is non-empty, it is inserted at point."
   (interactive "p")
   (dotimes (i count)
     (windmove-right)))
-(global-set-key (kbd "H-l") 'hyper-window-right)
 
 (defun hyper-window-up (count)
   "Move the cursor to new COUNT-th window above the current one."
@@ -262,7 +188,6 @@ If the input is non-empty, it is inserted at point."
   (interactive "p")
   (dotimes (i (or count 1))
     (windmove-up)))
-(global-set-key (kbd "H-k") 'hyper-window-up)
 
 (defun hyper-window-down (count)
   "Move the cursor to new COUNT-th window below the current one."
@@ -270,7 +195,49 @@ If the input is non-empty, it is inserted at point."
   (interactive "p")
   (dotimes (i (or count 1))
     (windmove-down)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Keyboard Macros
+(fset 'setupworkspace
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("aao23iooopo" 0 "%d")) arg)))
+(global-set-key (kbd "C-x C-k 1") 'setupworkspace)
+
+(fset 'OHSFigureSave
+   [?# ?+ ?C ?A ?P ?T ?I ?O ?N ?: ?  ?\C-x ?Q return return tab ?\[ ?\[ ?f ?i ?l ?e ?: ?. ?/ ?W ?e ?e ?k ?  ?\C-x ?Q return ?/ ?\C-x ?Q return ?_ ?\C-u ?\M-! ?d ?a ?t ?e ?  ?+ ?% ?H ?% ?M ?% ?S return escape ?e ?a ?. ?p ?n ?g escape ?v ?B ?F ?/ ?l ?y escape ?A ?\] ?\] return escape ?p ?0 ?i ?\M-x ?i ?n ?s ?e ?r ?t ?d ?i ?r ?e ?c ?t ?o ?r ?y return escape ?V ?d ?i ?\C-x ?\C-f ?\C-  ?\C-a backspace ?/ ?U ?s ?e ?r ?s ?/ ?p ?i ?e ?r ?c ?e ?w ?a ?n ?g ?/ ?S ?c ?r ?e ?e ?n ?s ?h ?o ?t ?s return ?s ?\M-< ?/ ?S ?c ?r ?e ?e ?n ?  ?S ?h ?o ?t return ?R ?\C-  ?\C-a backspace ?\s-v backspace return ?\C-x ?k return])
+(global-set-key (kbd "<f9>") 'OHSFigureSave)
+
+(fset 'importChineseFlashcards
+   [return ?\C-p ?* ?* ?  ?I ?t ?e ?m ?\C-c ?\C-c ?d ?r ?i ?l ?l return ?\C-n ?\C-a ?\C-z ?f ?= ?x ?x ?\C-z ?\C-k ?\C-n ?\C-a return return ?\C-p ?* ?* ?  ?A ?n ?s ?w ?e ?r ?\C-a ?* ?\C-n ?\C-a ?\C-y ?\; ?  ?\C-a ?\C-n ?\C-n])
+(global-set-key (kbd "<f6>") 'importChineseFlashcards)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Run Emacs as Daemon
+(server-start)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ERC
+(setq erc-log-channels-directory "~/logs/")
+(setq erc-save-buffer-on-part t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Keybinds
+;; Set keys only for Mac OSX
+(when (eq system-type 'darwin)
+    (setq mac-option-modifier 'meta)
+    (setq mac-control-modifier 'control)
+    (setq ns-function-modifier 'hyper))
+
+;;; Moving Windows with Hyper (Fn)
+(global-set-key (kbd "H-h") 'hyper-window-left)
+(global-set-key (kbd "H-l") 'hyper-window-right)
+(global-set-key (kbd "H-k") 'hyper-window-up)
 (global-set-key (kbd "H-j") 'hyper-window-down)
 
-;; Keybinds
+;;; Visual Line Mode
 (global-set-key (kbd "H-v") 'visual-line-mode)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
---
+++ init_files/custom.el
@@ -0,0 +1,75 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-enabled-themes (quote (smart-mode-line-powerline)))
+ '(custom-safe-themes
+   (quote
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(default-input-method "TeX")
+ '(epg-gpg-program "/usr/local/bin/gpg")
+ '(erc-modules
+   (quote
+    (autojoin button completion fill irccontrols list log match menu move-to-prompt netsplit networks noncommands readonly ring stamp track)))
+ '(org-agenda-files
+   (list "~/Dropbox/org/school.org" "~/Dropbox/org/todo.org" "~/Dropbox/org/violin.org"))
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill org-learn)))
+ '(org-structure-template-alist
+   (quote
+    (("s" "#+BEGIN_SRC ?
+
+#+END_SRC")
+     ("e" "#+BEGIN_EXAMPLE
+?
+#+END_EXAMPLE")
+     ("q" "#+BEGIN_QUOTE
+?
+#+END_QUOTE")
+     ("v" "#+BEGIN_VERSE
+?
+#+END_VERSE")
+     ("V" "#+BEGIN_VERBATIM
+?
+#+END_VERBATIM")
+     ("c" "#+BEGIN_CENTER
+?
+#+END_CENTER")
+     ("C" "#+BEGIN_COMMENT
+?
+#+END_COMMENT")
+     ("l" "#+BEGIN_EXPORT latex
+?
+#+END_EXPORT")
+     ("L" "#+LaTeX: ")
+     ("h" "#+BEGIN_EXPORT html
+?
+#+END_EXPORT")
+     ("H" "#+HTML: ")
+     ("a" "#+BEGIN_EXPORT ascii
+?
+#+END_EXPORT")
+     ("A" "#+ASCII: ")
+     ("i" "#+INDEX: ?")
+     ("I" "#+INCLUDE: %file ?"))))
+ '(package-selected-packages
+   (quote
+    (elpy exec-path-from-shell smart-mode-line-powerline-theme smart-mode-line latex auctex evil-visual-mark-mode)))
+ '(safe-local-variable-values
+   (quote
+    ((eval face-remap-add-relative
+	   (quote default)
+	   :family "Arial"))))
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
