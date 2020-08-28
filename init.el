5907f1b723800acbe80d096f644c18763accaf4e
piercegwang <piercegwang@users.noreply.github.com>
2020-08-27 22:18:45 -0700

    Configured mu4e

--- init.el
+++ init.el
@@ -553,6 +553,21 @@ SCHEDULED: %^{Scheduled time + duration}T
 :LOGGED: %^{Logged Time}U
 :END:
 " :kill-buffer t)
+("m" "Mail")
+("mt" "Mail task" entry
+ (file "~/Dropbox/org/a_inbox.org")
+ "* NEXT %? :email:
+:PROPERTIES:
+:EMAIL: %a
+:SENDER: %:from
+:END:\n")
+("mn" "Mail note" entry
+ (file "~/Dropbox/org/a_inbox.org")
+ "* %? :email:
+:PROPERTIES:
+:EMAIL: %a
+:SENDER: %:from
+:END:\n")
 ("S" "School")
 ("St" "School Tasks")
 ("Stj" "OCS15 - T/Th" entry
@@ -989,6 +1004,188 @@ If the input is non-empty, it is inserted at point."
 (put 'narrow-to-region 'disabled nil)
 
 (setq browse-url-firefox-program "/Applications/Firefox.app/Contents/MacOS/firefox-bin")
+
+; add the source shipped with mu to load-path
+(add-to-list 'load-path (expand-file-name "/usr/local/Cellar/mu/1.4.13/share/emacs/site-lisp/mu/mu4e/"))
+
+; require mu4e
+(require 'mu4e)
+(global-set-key (kbd "H-m") 'mu4e)
+
+(setq mu4e-maildir (expand-file-name "~/Maildir"))
+
+; get mail
+(setq mu4e-get-mail-command "mbsync -c ~/.emacs.d/mu4e/.mbsyncrc -a"
+  ;; mu4e-html2text-command "w3m -T text/html" ;;using the default mu4e-shr2text
+  mu4e-view-prefer-html t
+  mu4e-update-interval 180
+  mu4e-headers-auto-update t
+  mu4e-compose-signature-auto-include nil
+  mu4e-compose-format-flowed t); tell mu4e to use w3m for html rendering
+
+;; enable inline images
+(setq mu4e-view-show-images t)
+
+;; from info manual
+(add-to-list 'mu4e-view-actions
+             '("ViewInBrowser" . mu4e-action-view-in-browser) t)
+
+
+;; <tab> to navigate to links, <RET> to open them in browser
+(add-hook 'mu4e-view-mode-hook
+          (lambda()
+            ;; try to emulate some of the eww key-bindings
+            (local-set-key (kbd "<RET>") 'mu4e~view-browse-url-from-binding)
+            (local-set-key (kbd "<tab>") 'shr-next-link)
+            (local-set-key (kbd "<backtab>") 'shr-previous-link)))
+
+;; from https://www.reddit.com/r/emacs/comments/bfsck6/mu4e_for_dummies/elgoumx
+(add-hook 'mu4e-headers-mode-hook
+      (defun my/mu4e-change-headers ()
+        (interactive)
+        (setq mu4e-headers-fields
+              `((:human-date . 25) ;; alternatively, use :date
+                (:flags . 6)
+                (:from . 22)
+                (:thread-subject . ,(- (window-body-width) 70)) ;; alternatively, use :subject
+                (:size . 7)))))
+
+;; if you use date instead of human-date in the above, use this setting
+;; give me ISO(ish) format date-time stamps in the header list
+;(setq mu4e-headers-date-format "%Y-%m-%d %H:%M")
+
+;; spell check
+(add-hook 'mu4e-compose-mode-hook
+    (defun my-do-compose-stuff ()
+       "My settings for message composition."
+       (visual-line-mode)
+       (org-mu4e-compose-org-mode)
+           (use-hard-newlines -1)
+           (flyspell-mode)))
+
+(add-hook 'mu4e-view-mode-hook #'visual-line-mode)
+
+;; every new email composition gets its own frame!
+(setq mu4e-compose-in-new-frame t)
+
+(require 'smtpmail)
+
+;;rename files when moving
+;;NEEDED FOR MBSYNC
+(setq mu4e-change-filenames-when-moving t)
+
+;;set up queue for offline email
+;;use mu mkdir  ~/Maildir/acc/queue to set up first
+(setq smtpmail-queue-mail nil)  ;; start in normal mode
+
+;;from the info manual
+(setq mu4e-attachment-dir  "~/Documents")
+
+(setq message-kill-buffer-on-exit t)
+(setq mu4e-compose-dont-reply-to-self t)
+
+(require 'org-mu4e)
+
+;; convert org mode to HTML automatically
+(setq org-mu4e-convert-to-html t)
+
+;;from vxlabs config
+;; show full addresses in view message (instead of just names)
+;; toggle per name with M-RET
+(setq mu4e-view-show-addresses 't)
+
+;; don't ask when quitting
+(setq mu4e-confirm-quit nil)
+
+;; mu4e-context
+(setq mu4e-context-policy 'pick-first)
+(setq mu4e-compose-context-policy 'always-ask)
+(setq mu4e-contexts
+  (list
+   (make-mu4e-context
+    :name "personal" ;;for pierce.g.wang
+    :enter-func (lambda () (mu4e-message "Entering context personal"))
+    :leave-func (lambda () (mu4e-message "Leaving context personal"))
+    :match-func (lambda (msg)
+                  (when msg
+                (mu4e-message-contact-field-matches
+                 msg '(:from :to :cc :bcc) "pierce.g.wang@gmail.com")))
+    :vars '((user-mail-address . "pierce.g.wang@gmail.com")
+            (user-full-name . "Pierce Wang")
+            (mu4e-sent-folder . "/pierce.g.wang/[pierce.g.wang].Sent Mail")
+            (mu4e-drafts-folder . "/pierce.g.wang/[pierce.g.wang].drafts")
+            (mu4e-trash-folder . "/pierce.g.wang/[pierce.g.wang].Trash")
+            (mu4e-compose-signature . (concat "Formal Signature\n" "Emacs 27, org-mode 9, mu4e 1.14\n"))
+            (mu4e-compose-format-flowed . t)
+            (smtpmail-queue-dir . "~/Maildir/pierce.g.wang/queue/cur")
+            (message-send-mail-function . smtpmail-send-it)
+            (smtpmail-smtp-user . "pierce.g.wang")
+            (smtpmail-starttls-credentials . (("smtp.gmail.com" 587 nil nil)))
+            (smtpmail-auth-credentials . (expand-file-name "~/.authinfo.gpg"))
+            (smtpmail-default-smtp-server . "smtp.gmail.com")
+            (smtpmail-smtp-server . "smtp.gmail.com")
+            (smtpmail-smtp-service . 587)
+            (smtpmail-debug-info . t)
+            (smtpmail-debug-verbose . t)
+            (mu4e-maildir-shortcuts . ( ("/pierce.g.wang/INBOX"            . ?i)
+                                        ("/pierce.g.wang/[pierce.g.wang].Sent Mail" . ?s)
+                                        ("/pierce.g.wang/[pierce.g.wang].Bin"       . ?t)
+                                        ("/pierce.g.wang/[pierce.g.wang].All Mail"  . ?a)
+                                        ("/pierce.g.wang/[pierce.g.wang].Starred"   . ?r)
+                                        ("/pierce.g.wang/[pierce.g.wang].drafts"    . ?d)
+                                        ))))
+      (make-mu4e-context
+       :name "work" ;;for pierce.wang.violin
+       :enter-func (lambda () (mu4e-message "Entering context personal"))
+       :leave-func (lambda () (mu4e-message "Leaving context personal"))
+       :match-func (lambda (msg)
+                     (when msg
+                       (mu4e-message-contact-field-matches
+                        msg '(:from :to :cc :bcc) "pierce.wang.violin@gmail.com")))
+       :vars '((user-mail-address . "pierce.wang.violin@gmail.com")
+               (user-full-name . "Pierce Wang")
+               (mu4e-sent-folder . "/pierce.wang.violin/[pierce.wang.violin].Sent Mail")
+               (mu4e-drafts-folder . "/pierce.wang.violin/[pierce.wang.violin].drafts")
+               (mu4e-trash-folder . "/pierce.wang.violin/[pierce.wang.violin].Trash")
+               (mu4e-compose-signature . (concat "Formal Signature\n" "Emacs 27, org-mode 9, mu4e 1.14\n"))
+               (mu4e-compose-format-flowed . t)
+               (smtpmail-queue-dir . "~/Maildir/pierce.wang.violin/queue/cur")
+               (message-send-mail-function . smtpmail-send-it)
+               (smtpmail-smtp-user . "pierce.wang.violin")
+               (smtpmail-starttls-credentials . (("smtp.gmail.com" 587 nil nil)))
+               (smtpmail-auth-credentials . (expand-file-name "~/.authinfo.gpg"))
+               (smtpmail-default-smtp-server . "smtp.gmail.com")
+               (smtpmail-smtp-server . "smtp.gmail.com")
+               (smtpmail-smtp-service . 587)
+               (smtpmail-debug-info . t)
+               (smtpmail-debug-verbose . t)
+               (mu4e-maildir-shortcuts . ( ("/pierce.wang.violin/INBOX"            . ?i)
+                                           ("/pierce.wang.violin/[pierce.wang.violin].Sent Mail" . ?s)
+                                           ("/pierce.wang.violin/[pierce.wang.violin].Bin"       . ?t)
+                                           ("/pierce.wang.violin/[pierce.wang.violin].All Mail"  . ?a)
+                                           ("/pierce.wang.violin/[pierce.wang.violin].Starred"   . ?r)
+                                           ("/pierce.wang.violin/[pierce.wang.violin].drafts"    . ?d)
+                                           ))))
+      ))
+
+(use-package mu4e-alert
+  :ensure t
+  :after mu4e
+  :init
+  (setq mu4e-alert-interesting-mail-query
+        (concat
+         "flag:unread maildir:/Exchange/INBOX "
+         "OR "
+         "flag:unread maildir:/Gmail/INBOX"
+         ))
+  (mu4e-alert-enable-mode-line-display)
+  (defun pgw/refresh-mu4e-alert-mode-line ()
+    (interactive)
+    (mu4e~proc-kill)
+    (mu4e-alert-enable-mode-line-display)
+    )
+  (run-with-timer 0 60 'pgw/refresh-mu4e-alert-mode-line)
+  )
 
 (defun pgw/date-block (absolute y1 m1 d1 y2 m2 d2)
   "Block date entry. An adapted version of the `diary-block'
--- transient/history.el
+++ transient/history.el
@@ -3,4 +3,5 @@
  (magit-dispatch nil)
  (magit-merge nil)
  (magit-pull nil)
- (magit-push nil))
+ (magit-push nil)
+ (magit-run nil))
